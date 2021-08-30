338c95313d28f51d2604a40bfa76aa1dbdb141d8
BadNarrators <andreaatzei01@gmail.com>
2021-08-30 05:41:07 +0200

    Initial Commit

---
+++ IDESenpai/IDESenpaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDESenpaiApp.swift
+//  IDESenpai
+//
+//  Created by Andrea Atzei on 30/08/21.
+//
+
+import SwiftUI
+
+@main
+struct IDESenpaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
